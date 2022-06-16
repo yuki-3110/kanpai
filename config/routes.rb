08d26e8f85451b70196a03a724369f61da4e1ee5
yuki saito <ykst0622@gmail.com>
2022-06-16 16:04:10 +0900

    Pictureモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pictures
   root 'users#new'
   resources :users, only: %i(new create show edit destroy)
   resources :sessions, only: %i(new create destroy)
