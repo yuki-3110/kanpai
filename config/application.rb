18a8ec35c9eba747aac0e617055d2442dab30b69
yuki saito <ykst0622@gmail.com>
2022-06-16 09:38:47 +0900

    辞書機能・エラーメッセージ・タイムゾーン等設定

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,16 @@ Bundler.require(*Rails.groups)
 
 module Kanpai
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.generators do |g|
+
+      g.assets false
+      g.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
