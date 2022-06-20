4aa3704fda78dffdd25a231877e81b2f22d92907
yuki saito <ykst0622@gmail.com>
2022-06-20 23:27:44 +0900

    ログアウト機能・エラーメッセージ機能・シードデータ実装

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -2,7 +2,7 @@ module PicturesHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'create'
       confirm_pictures_path
-    else action_name == 'edit'
+    elsif action_name == 'edit'
       picture_path
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-50. times do |n|
-  name = Faker::Account.name
+50.times do |n|
+  name = Faker::Avatar.name
   email = Faker::Internet.email
   password = "password"
   User.create!(name: name, email: email, password: password,)
