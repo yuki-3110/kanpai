8ab1d347386b0684b55662d8dae4f59766588251
yuki saito <ykst0622@gmail.com>
2022-06-21 00:20:54 +0900

    checksテーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_19_145252) do
+ActiveRecord::Schema.define(version: 2022_06_20_151751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "checks", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
